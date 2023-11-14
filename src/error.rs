16aa2b9f219eb3d4476c7f9128b786ebd3889e0d
HimbeerserverDE <himbeerserverde@gmail.com>
2023-11-14 18:01:28 +0100

    remove unused / binary error variants

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use std::{ffi, io, net};
+use std::{ffi, io};
 
 use thiserror::Error;
 
@@ -6,22 +6,13 @@ use thiserror::Error;
 pub enum Error {
     #[error("link {0} not found")]
     LinkNotFound(String),
-    #[error("not enough ipv6 subnets")]
-    NotEnoughIpv6Subnets,
 
     #[error("ffi nul: {0}")]
     Nul(#[from] ffi::NulError),
     #[error("io: {0}")]
     Io(#[from] io::Error),
-
-    #[error("ipnet prefix len: {0}")]
-    IpnetPrefixLen(#[from] ipnet::PrefixLenError),
-    #[error("net: parse ip address: {0}")]
-    NetAddrParseError(#[from] net::AddrParseError),
     #[error("rtnetlink: {0}")]
     RtNetlink(#[from] rtnetlink::Error),
-    #[error("serde_json: {0}")]
-    SerdeJson(#[from] serde_json::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
